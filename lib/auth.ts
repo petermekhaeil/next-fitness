af3887d313a4bab72d08b2300f3371ba0c748c6b
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2024-01-03 20:19:11 +0800

    next-auth@beta

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,7 @@
 import NextAuth from "next-auth";
 import StravaProvider from "next-auth/providers/strava";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     StravaProvider({
       clientId: process.env.STRAVA_CLIENT_ID,
