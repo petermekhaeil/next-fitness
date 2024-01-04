081577efc370cfb98fdea3421a38ebff5e9bf7fc
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2024-01-04 09:20:11 +0800

    test server fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import LoginForm from "@/components/login-form";
-import { auth } from "@/lib/auth";
+import { auth, signOut } from "@/lib/auth";
 import type { Session } from "next-auth";
 
 export default async function Page() {
@@ -11,12 +11,27 @@ export default async function Page() {
   );
 }
 
-export function SessionData({ session }: { session: Session | null }) {
+function SessionData({ session }: { session: Session | null }) {
   if (session?.user) {
+    const activities = fetch("https://www.strava.com/api/v3/activities", {
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    }).then((res) => res.json());
+
     return (
       <div className="w-full space-y-2 overflow-auto">
         <h2 className="text-xl font-bold">Current Session Data</h2>
         <pre>{JSON.stringify(session, null, 2)}</pre>
+        <pre>{JSON.stringify(activities, null, 2)}</pre>
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit">Sign out</button>
+        </form>
       </div>
     );
   }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,46 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import StravaProvider from "next-auth/providers/strava";
+
+declare module "@auth/core/types" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+    accessToken: string;
+  }
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     StravaProvider({
       clientId: process.env.STRAVA_CLIENT_ID,
       clientSecret: process.env.STRAVA_CLIENT_SECRET,
+      authorization: {
+        url: "https://www.strava.com/api/v3/oauth/authorize",
+        params: {
+          scope: "activity:read_all",
+          approval_prompt: "auto",
+          response_type: "code",
+        },
+      },
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      // @ts-expect-error fix typing
+      session.user.id = token.id;
+      // @ts-expect-error fix typing
+      session.accessToken = token.accessToken;
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
 });
